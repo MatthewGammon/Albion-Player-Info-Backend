d7a4c0d9b2dd46087ba09d56eb417973c8fd0a7c
Matthew Gammon <mgammon91@gmail.com>
2022-02-17 20:32:30 -0500

    create migration file and build regears table

---
+++ src/db/migrations/20220218012509_createRegearsTable.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('regears', (table) => {
+    table.increments('regear_id').primary();
+    table.string('event_id').notNullable();
+    table.string('character_name').notNullable();
+    table.string('head_piece').notNullable();
+    table.string('chest_armor').notNullable();
+    table.string('shoes').notNullable();
+    table.string('main_hand').notNullable();
+    table.string('status').notNullable().defaultTo('pending');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('regears');
+};
