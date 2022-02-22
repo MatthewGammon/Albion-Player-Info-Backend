fb5d3226baff8514898304c06621c3a15b3b3cae
Matthew Gammon <mgammon91@gmail.com>
2022-02-21 22:24:56 -0500

    import and use errorHandler middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 const express = require('express');
 const cors = require('cors');
 
+const errorHandler = require('./errors/errorHandler');
 const regearsRouter = require('./regears/regears.router');
 
 const app = express();
@@ -14,4 +15,5 @@ app.use(express.json());
 
 app.use('/regears', regearsRouter);
 
+app.use(errorHandler);
 module.exports = app;
